407b1f72bda463f4a84fb29c7017c877813acdb1
Acer <madubhashinimanusha@gmail.com>
2025-12-05 13:33:08 +0530

    add movie creation dialog, form UI and utility helpers

--- app/dashboard/movies/page.jsx
+++ app/dashboard/movies/page.jsx
@@ -1,17 +1,18 @@
+import AddMoviesFrom from "@/components/add-movies-from";
 import MoviesData from "./movies-data";
+import AddMoviesDialog from "@/components/add-movies-dialog";
 
 export default function MoviePage() {
-    return ( 
-        <div className="space-y-4">
-            <div className="flex items-center justify-between">
-                <div>
-                    <h2 className="text-xl font-bold tracking-tight">Movies</h2>
-                    <p className="text-muted-foreground">Manage your movies catalog</p>
-                </div>
-
-            </div>
-            <MoviesData/>
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <div>
+          <h2 className="text-xl font-bold tracking-tight">Movies</h2>
+          <p className="text-muted-foreground">Manage your movies catalog</p>
         </div>
-    )
-
+        <AddMoviesDialog />
+      </div>
+      <MoviesData />
+    </div>
+  );
 }
